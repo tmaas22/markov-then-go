e7c20595b46294476f8f4f6ce1c90624c4608d06
Thomas Matrejek <tamatrej@ncsu.edu>
2017-09-30 10:01:30 -0400

    Create initial Words types for tracking probabilities t

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+type Meh struct {
+	TheList map[string]WordsOnWords
+}
+
+type WordsOnWords struct {
+	Followers map[string]int
+	Total     int
+}
+
 func main() {
 	fmt.Println("Yay! We back in go!")
 }
